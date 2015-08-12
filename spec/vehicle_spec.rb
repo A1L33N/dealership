0a00ee5f79e1314af646cb3c12456daaddf3e9ad
Aileen Cacayorin & Janice Laset-Parkerson <student@epicodus.com>
2015-08-12 09:24:04 -0700

    add make, model, and year methods

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,9 +2,24 @@ require('rspec')
 require('vehicle')
 
 describe('Vehicle') do
+  before() do
+    @test_car = Vehicle.new('Ford', 'Escape', 2013)
+  end
   describe '#make' do
     it('it returns the make of the car') do
-      test_car = Vehicle.new('Ford', 'Escape', 2013)
+      expect(@test_car.make).to eq 'Ford'
+    end
+  end
+
+  describe '#model' do
+    it('returns the model of the car') do
+      expect(@test_car.model).to eq 'Escape'
+    end
+  end
+
+  describe '#year' do
+    it('returns the year of the car') do
+      expect(@test_car.year).to eq 2013
     end
   end
 end
